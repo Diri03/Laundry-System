6b97e3ea77eda4925c983ab2f80db732be42f820
Diri03 <diriansyah15@gmai.com>
2025-06-21 14:57:00 +0700

    edited dashboard and report

--- content/dashboard.php
+++ content/dashboard.php
@@ -2,9 +2,18 @@
     $queryUser = mysqli_query($conn, "SELECT * FROM user");
     $countUser = mysqli_num_rows($queryUser);
 
+    $queryCustomer = mysqli_query($conn, "SELECT * FROM customer");
+    $countCustomer = mysqli_num_rows($queryCustomer);
+
+    $queryService = mysqli_query($conn, "SELECT * FROM type_of_service");
+    $countService = mysqli_num_rows($queryService);
+
     $id = $_SESSION['ID_USER'];
     $queryLogin = mysqli_query($conn, "SELECT user.*, level.level_name FROM user LEFT JOIN level ON user.id_level = level.id WHERE user.id = '$id'");
     $rowLogin = mysqli_fetch_assoc($queryLogin);
+
+    $queryOrder = mysqli_query($conn, "SELECT c.customer_name, o.* FROM trans_order o LEFT JOIN customer c ON o.id_customer = c.id ORDER BY total DESC LIMIT 20");
+    $rowOrder = mysqli_fetch_all($queryOrder, MYSQLI_ASSOC);
 ?>
 
 <div class="container-fluid flex-grow-1 container-p-y">
@@ -16,33 +25,75 @@
                     <h3 class="card-title" align="center">Dashboard</h3>
                     <div class="row mb-3">
                         <div class="col-sm-4 text-center">
-                            <div class="card">
-                                <div class="card-body">
-                                    <h5 class="card-title" align="center">Account Login</h5>
-                                    <div style="text-align: justify">
-                                        Name : <?php echo $rowLogin['name']; ?><br>
-                                        Level : <?php echo $rowLogin['level_name']; ?><br>
-                                        Email : <?php echo $rowLogin['email']; ?><br>
-                                        Login Date : <?php echo date('d F Y'); ?><br>
+                            <div class="mb-3">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <h5 class="card-title" align="center">Account Login</h5>
+                                        <div class="mb-3">
+                                            <img src="assets/assets/img/avatars/1.png" alt="alt" width="50%" />
+                                        </div>
+                                        <div style="text-align: justify">
+                                            Name : <?php echo $rowLogin['name']; ?><br>
+                                            Level : <?php echo $rowLogin['level_name']; ?><br>
+                                            Email : <?php echo $rowLogin['email']; ?><br>
+                                            Login Date : <?php echo date('d F Y'); ?><br>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
+                        <div class="col-sm-8">
+                            <div class="table-responsive">
+                                <table class="table table-bordered">
+                                    <thead>
+                                        <tr>
+                                            <th>No</th>
+                                            <th>Name</th>
+                                            <th>Date</th>
+                                            <th>Status</th>
+                                            <th>Total</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <?php foreach ($rowOrder as $key => $data) { ?>
+                                            <tr>
+                                                <td><?php echo $key + 1; ?></td>
+                                                <td><?php echo $data['customer_name']; ?></td>
+                                                <td><?php echo tanggal($data['order_date']); ?></td>
+                                                <td><?php echo $data['order_status'] == 1 ? 'Pickup' : 'Process'; ?></td>
+                                                <td><?php echo $data['total']; ?></td>
+                                            </tr>
+                                        <?php } ?>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
                     </div>
-                    <div class="row">
+                    <div class="row mb-3">
+                        <div class="col-sm-4">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h5 class="card-title" align="center">Data Customer</h5>
+                                    <h1 align="center"><?php echo $countCustomer; ?></h1>
+                                </div>
+                            </div>                            
+                        </div>
+                        <div class="col-sm-4">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h5 class="card-title" align="center">Data Service</h5>
+                                    <h1 align="center"><?php echo $countService; ?></h1>
+                                </div>
+                            </div>                            
+                        </div>
                         <div class="col-sm-4">
                             <div class="card">
                                 <div class="card-body">
                                     <h5 class="card-title" align="center">Data User</h5>
                                     <h1 align="center"><?php echo $countUser; ?></h1>
-                                    <div class="mb-3" align="center">
-                                        <a href="?page=user" class="btn btn-primary">More Info</a>
-                                    </div>
                                 </div>
-                            </div>
+                            </div>                            
                         </div>
-                        <div class="col-sm-4"></div>
-                        <div class="col-sm-4"></div>
                     </div>
                 </div>
             </div>
--- content/report.php
+++ content/report.php
@@ -1,14 +1,29 @@
 <?php
+    $selectOrder = mysqli_query($conn, "SELECT order_date FROM trans_order ORDER BY order_date");
+    $rowOrder = mysqli_fetch_all($selectOrder, MYSQLI_ASSOC);
+    $len = mysqli_num_rows($selectOrder);
+    $date_min = $rowOrder[0]['order_date'];
+    $date_max = $rowOrder[$len - 1]['order_date'];
+
+    if (isset($_POST['filter'])) {
+        $start = $_POST['date_start'];
+        $end = $_POST['date_end'];
+    } else {
+        $start = $date_min;
+        $end = $date_max;
+    }
+
     $q = mysqli_query($conn, "SELECT c.customer_name, o.order_date, s.service_name, od.qty, s.price, od.subtotal
     FROM trans_order_detail od
     LEFT JOIN type_of_service s ON od.id_service = s.id
     LEFT JOIN trans_order o ON od.id_order = o.id
     LEFT JOIN customer c ON o.id_customer = c.id
-
+    WHERE o.order_date BETWEEN '$start' AND '$end'
     ORDER BY od.id, o.total DESC
     ");
 
     $r = mysqli_fetch_all($q, MYSQLI_ASSOC);
+
 ?>
 
 <div class="row">
@@ -22,14 +37,14 @@
                             <div class="row">
                                 <div class="col-sm-3">
                                     <label for="" class="form-label">Date Start</label>
-                                    <input type="date" name="date_start" class="form-control" required>
+                                    <input type="date" name="date_start" class="form-control" value="<?php echo $start; ?>" required>
                                 </div>
                                 <div class="col-sm-3">
                                     <label for="" class="form-label">Date End</label>
-                                    <input type="date" name="date_end" class="form-control" required>
+                                    <input type="date" name="date_end" class="form-control" value="<?php echo $end; ?>" required>
                                 </div>
                                 <div class="col-sm-3 mt-8">
-                                    <button type="submit" class="btn btn-primary">Filter</button>
+                                    <button type="submit" name="filter" class="btn btn-primary">Filter</button>
                                 </div>
                             </div>
                         </form>
